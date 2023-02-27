66d53c6dfac3d8e3ab3536b0ce4a1fd6d104203c
pivotiiii <17112987+pivotiiii@users.noreply.github.com>
2023-02-27 14:17:57 +0100

    change buildtool, add tests, change ctrtool ver

---
+++ src/compile.bat
@@ -0,0 +1,21 @@
+@echo off
+ROBOCOPY tools "../build/tools"
+ROBOCOPY . "../build" main.py
+ROBOCOPY . "../build" tests.py
+cd "..\build
+REM py -3-32 -m nuitka --onefile --standalone --include-data-dir="D:\Documents\VS Code\nsui_banner_fixer\build\tools=tools" -o nsui_banner_fixer.exe main.py
+py -3-32 -m nuitka --onefile --standalone -o nsui_banner_fixer.exe main.py
+py tests.py
+IF /I "%ERRORLEVEL%" NEQ "1" (
+    goto zip
+)
+goto err
+:zip
+echo:
+zip nsui_banner_fixer.zip nsui_banner_fixer.exe tools/*
+echo zip created
+goto end
+:err
+pause
+:end
+cd "..\src
