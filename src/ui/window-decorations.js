1a3dd1c0d3c92ce62ee543e142c0e03ff3df3e9e
pivotiiii <17112987+pivotiiii@users.noreply.github.com>
2024-11-14 19:34:00 +0100

    dark light change with css instead of js

--- src/ui/window-decorations.js
+++ src/ui/window-decorations.js
@@ -1,6 +1,5 @@
 const getElementsDeco = ids => Object.assign({}, ...ids.map(id => ({ [id]: document.getElementById(id) })));
 const uiDeco = getElementsDeco(["closeButton", "minButton", "maxButton", "windowBarCenter", "colorButton"]);
-const r = document.querySelector(":root");
 
 uiDeco.closeButton.addEventListener("click", async () => {
     saucer.exposed.quit();
@@ -17,23 +16,7 @@ uiDeco.minButton.addEventListener("click", async () => {
 uiDeco.colorButton.addEventListener("click", async () => {
     if (Appearance.getColorScheme() === "dark") {
         Appearance.setLightScheme()
-        r.style.setProperty("--window-bar-background-color", "rgb(255, 255, 255)");
-        uiDeco.colorButton.children[0].classList.remove("icons10-sun");
-        uiDeco.colorButton.children[0].classList.add("icons10-moon");
     } else {
         Appearance.setDarkScheme()
-        r.style.setProperty("--window-bar-background-color", "rgb(17, 17, 17)");
-        uiDeco.colorButton.children[0].classList.remove("icons10-moon");
-        uiDeco.colorButton.children[0].classList.add("icons10-sun");
     }
 });
-
-if (Appearance.getColorScheme() === "dark") {
-    r.style.setProperty("--window-bar-background-color", "rgb(17, 17, 17)");
-    uiDeco.colorButton.children[0].classList.remove("icons10-moon");
-    uiDeco.colorButton.children[0].classList.add("icons10-sun");
-} else {
-    r.style.setProperty("--window-bar-background-color", "rgb(255, 255, 255)");
-    uiDeco.colorButton.children[0].classList.remove("icons10-sun");
-    uiDeco.colorButton.children[0].classList.add("icons10-moon");
-}
